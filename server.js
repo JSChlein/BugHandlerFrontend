7ddbf459ec6d9b73044bf14e0baeeaf16a5d3122
Jacob Schlein <jacobschlein@gmail.com>
2021-04-10 12:37:53 +0200

    filnavn update

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express')
 const port = process.env.PORT || 8080;
 const app = express()
 
-app.use(express.static(__dirname + "/client/dist"))
+app.use(express.static(__dirname + "/dist"))
 app.get(/.*/, function(req, res) {
-    res.sendFile(__dirname + "/client/dist/index.html")
+    res.sendFile(__dirname + "/dist/index.html")
 })
 app.listen(port);
 
